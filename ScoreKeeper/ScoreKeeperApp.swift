2f005cec0da870dbc3c5d2218b0478457777316f
David Okun <david@okun.io>
2020-09-29 08:58:31 -0500

    Now adding test script

--- ScoreKeeper/ScoreKeeperApp.swift
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -1,17 +1,17 @@
-////
-////  ScoreKeeperApp.swift
-////  ScoreKeeper
-////
-////  Created by David Okun on 9/27/20.
-////
 //
-//import SwiftUI
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
 //
-//@main
-//struct ScoreKeeperApp: App {
-//    var body: some Scene {
-//        WindowGroup {
-//          ScoreList(viewModel: ScoreListViewModel())
-//        }
-//    }
-//}
+//  Created by David Okun on 9/27/20.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+          ScoreList(viewModel: ScoreListViewModel())
+        }
+    }
+}

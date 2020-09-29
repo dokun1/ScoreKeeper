c69bc7d1c896002ce483b6d8e4d9bcc82523788d
David Okun <david@okun.io>
2020-09-28 22:09:53 -0500

    Updated app to work on iOS 13

--- ScoreKeeper/ScoreKeeperApp.swift
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -1,17 +1,17 @@
+////
+////  ScoreKeeperApp.swift
+////  ScoreKeeper
+////
+////  Created by David Okun on 9/27/20.
+////
 //
-//  ScoreKeeperApp.swift
-//  ScoreKeeper
+//import SwiftUI
 //
-//  Created by David Okun on 9/27/20.
-//
-
-import SwiftUI
-
-@main
-struct ScoreKeeperApp: App {
-    var body: some Scene {
-        WindowGroup {
-          ScoreList(viewModel: ScoreListViewModel())
-        }
-    }
-}
+//@main
+//struct ScoreKeeperApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//          ScoreList(viewModel: ScoreListViewModel())
+//        }
+//    }
+//}

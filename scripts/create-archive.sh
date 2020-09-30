03ce67149d7ff6198ec9006f0f58d44af42a2a05
David Okun <david@okun.io>
2020-09-29 20:53:07 -0500

    Specifying export options differently.

--- scripts/create-archive.sh
+++ scripts/create-archive.sh
@@ -2,17 +2,17 @@
 
 set -eo pipefail
 
-xcodebuild -project ScoreKeeper.xcodeproj \
-           -scheme "ScoreKeeper" \
-           -sdk iphoneos \
-           -archivePath ./ScoreKeeper.xcarchive \
-           -configuration "ScoreKeeper" \
-           PROVISIONING_PROFILE="AdHocScoreKeeper" \
-           clean archive
-
 #xcodebuild -project ScoreKeeper.xcodeproj \
 #           -scheme "ScoreKeeper" \
 #           -sdk iphoneos \
 #           -archivePath ./ScoreKeeper.xcarchive \
 #           -configuration "ScoreKeeper" \
+#           PROVISIONING_PROFILE="AdHocScoreKeeper" \
 #           clean archive
+
+xcodebuild -project ScoreKeeper.xcodeproj \
+           -scheme "ScoreKeeper" \
+           -sdk iphoneos \
+           -archivePath ./ScoreKeeper.xcarchive \
+           -configuration "ScoreKeeper" \
+           clean archive
